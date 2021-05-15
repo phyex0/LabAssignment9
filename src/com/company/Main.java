bbd45365732aec8b5c5558c33bfa2602d08fee17
phyex0 <phyex0-halit_burak@outlook.com>
2021-05-15 16:46:30 +0300

    .

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,39 @@ package com.company;
 
 import java.io.File;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        final  String path1 = "src/com/company/tomsawyer.txt";
         HashMap<String, Integer> hashMap = new HashMap<>();
-        FileIO.readFile(path1,hashMap);
 
 
+        final String path1 = "src/com/company/dyssy10.txt";
+        final String path2 = "src/com/company/tomsawyer.txt";
+        final String path3 = "src/com/company/totc.txt";
+        FileIO.wordCounter(path1,hashMap);
+        FileIO.wordCounter(path2,hashMap);
+        FileIO.wordCounter(path3,hashMap);
+
+        //System.out.println(hashMap.toString());
+
+        System.out.println("The size of the hashMap is :"+hashMap.size());
+        System.out.println("Common words:\n------------------------------------");
+        FileIO.printCommons(hashMap);
+
+        //System.out.println(mapToString(hashMap));
+
+
+    }
+
+    public static <K, V> String mapToString(Map<K, V> map) {
+        return map.entrySet()
+                .stream()
+                .map(entry -> entry.getKey() + ":" + entry.getValue())
+                .collect(Collectors.joining("\n"));
     }
 
 }
@@ -23,42 +44,50 @@ public class Main {
 class FileIO{
 
     public static String compound(String[] arr){
-        if(arr.length == 0)
+        if(arr.length==0)
             return null;
-        else{
-            String word="";
-            for(String s : arr)
-                word+=s;
-            return word;
-        }
-
-
+        else if(arr.length==1)
+            return arr[0];
+        else if(arr.length ==2)
+            return arr[0]+arr[1];
+        else if(arr.length==3)
+            return arr[1]+arr[2];
+        else
+            return null;
     }
 
-    public static void readFile(String path, HashMap<String,Integer > hashMap){
+
+    public static void wordCounter(String path, HashMap<String,Integer>hashMap){
+        List<String> list = readFile(path);
+        for(String word:list){
+            if(word != null){
+                if(hashMap.containsKey(word))
+                    hashMap.put(word,hashMap.get(word)+1);
+                else
+                    hashMap.put(word,1);
+
+            }
+        }
+    }
+
+
+    public static List<String> readFile(String path){
+        List<String> list = new ArrayList<>();
         File f= new File(path);
         try{
             if(!f.exists())
                 f.createNewFile();
 
-            Scanner scanner = new Scanner(f);
+           Scanner scanner = new Scanner(f);
             String[] words;
             while(scanner.hasNext()){
                 words = scanner.next().toLowerCase().split("[^a-zA-Z’]");
                 String word= compound(words);
-                if(word != null){
-
-                   if(hashMap.containsKey(word))
-                       hashMap.put(word,hashMap.get(word)+1);
-
-                   else
-                       hashMap.put(word,1);
-
-               }
-
+                if(word != null)
+                    list.add(word);
             }
 
-
+            return list;
 
         }catch(IOException exception){
             exception.printStackTrace();
@@ -69,6 +98,20 @@ class FileIO{
         }
 
 
+        return null;
+    }
+
+
+    public static void printCommons(HashMap<String,Integer> hashMap){
+        final String path = "src/com/company/commonwords.txt";
+        List<String> list = readFile(path);
+
+        for(String word:list)
+            if(hashMap.containsKey(word))
+                System.out.println(word+" : "+hashMap.get(word));
+
 
     }
+
+
 }
